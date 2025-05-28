fc9e58978d580677c4e339d63d91d7b8ba33a63d
JoseManuelDuarteP <josduaper@alu.edu.gva.es>
2025-05-28 13:26:44 +0200

    intial commit

--- src/metodos.rb
+++ src/metodos.rb
@@ -1,2 +1,19 @@
 # frozen_string_literal: true
 
+def raiz_cuadrada(numero)
+  Math::sqrt(numero)
+end
+
+def positivo?(numero)
+  if numero > 0
+    true
+  elsif numero < 0
+    false
+  else
+    'Cero'
+  end
+end
+
+puts positivo?(0)
+
+puts raiz_cuadrada(4)
