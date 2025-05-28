8a65bbf9b1833ceebb8b122938ec667bac01a7aa
JoseManuelDuarteP <josduaper@alu.edu.gva.es>
2025-05-28 13:00:46 +0200

    intial commit

--- src/prueba_array.rb
+++ src/prueba_array.rb
@@ -1,2 +1,16 @@
 # frozen_string_literal: true
 
+frase_entera = []
+
+while true
+  puts 'Introduzca frase:'
+  frase = gets.chomp
+  if frase == ''
+    break
+  end
+  frase_entera.push(frase)
+end
+
+frase_entera.each do |frase|
+  print frase + " "
+end
