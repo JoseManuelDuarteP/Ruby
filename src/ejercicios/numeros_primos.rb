f961160ebce7262f8306022e1cd5de02b8c0fa37
JoseManuelDuarteP <josduaper@alu.edu.gva.es>
2025-06-02 12:02:07 +0200

    intial commit

--- src/ejercicios/numeros_primos.rb
+++ src/ejercicios/numeros_primos.rb
@@ -1,9 +1,35 @@
 # frozen_string_literal: true
 
-puts 'Numero inferior'
+puts 'Número inferior'
 num1 = gets.chomp.to_i
-puts 'Numero superior'
+puts 'Número superior'
 num2 = gets.chomp.to_i
 
 primos = []
 
+while num1 <= num2
+
+  if num1 < 2
+    num1 += 1
+    next
+  end
+
+  es_primo = true
+  iter = 2
+
+  while iter < num1
+    if num1 % iter == 0
+      es_primo = false
+      break
+    end
+    iter += 1
+  end
+
+  if es_primo
+    primos.push(num1)
+  end
+
+  num1 += 1
+end
+
+puts primos
